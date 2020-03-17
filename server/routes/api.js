765bd66b4591c97b2b813cde141e542e59541423
Luka <noobtenluka@gmail.com>
2020-03-17 18:20:00 +0100

    Made the server smaller because of unneeded stuff

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,14 +3,6 @@ const router = express.Router();
 const Request = require("../models/request");
 const checkAuth = require("../middlewares/check-auth");
 
-router.get("/requestswithoutplz", (req, res, next) => {
-  Request.find({ zip: "00000" }, "phone")
-    .limit(20)
-    .exec()
-    .then(docs => res.json(docs))
-    .catch(err => next(err));
-});
-
 router.post("/requests", checkAuth, (req, res, next) => {
   if (req.body.phone && req.body.zip && req.body.request) {
     Request.create({
@@ -23,19 +15,6 @@ router.post("/requests", checkAuth, (req, res, next) => {
   } else {
     res.json({
       error: "One or more input fields are empty"
-    });
-  }
-});
-
-router.delete("/request", checkAuth, (req, res, next) => {
-  if (req.body.id) {
-    Request.findByIdAndDelete(req.body.id)
-      .exec()
-      .then(() => res.send("Success"))
-      .catch(err => next(err));
-  } else {
-    res.json({
-      error: "No id"
     });
   }
 });
