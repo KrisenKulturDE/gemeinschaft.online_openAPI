dcf1b59f8f4764fc6b6245785d5eb641294cb6e7
Luka <noobtenluka@gmail.com>
2020-03-18 14:07:35 +0100

    Validated more numbers

--- src/middlewares/check-phone.ts
+++ src/middlewares/check-phone.ts
@@ -10,8 +10,21 @@ const checkPhone = (req: Request, res: Response, next: NextFunction) => {
 
     if (firstTwo === "00" && firstFour !== "0049") {
       return next(new HttpException(400, "Not a german number!"));
+    } else if (firstFour === "0049") {
+      firstFour = "0" + phoneNumber.substring(4, 7);
     }
-    next();
+
+    switch (firstFour) {
+      case "0137":
+      case "0700":
+      case "0900":
+      case "0180":
+      case "0190":
+      case "1180":
+        next(new HttpException(400, "Number not allowed"));
+      default:
+        next();
+    }
   } else {
     next(new HttpException(400, "No phone number provided"));
   }
